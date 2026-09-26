641ec29531adfb305404443eeeb90a57051c780d
suprajakommineni <suprajakommineni30@gmail.com>
2026-09-26 20:10:41 +0530

    fix backend cors configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,17 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: (origin: any, callback: any) => {
-      if (
-        !origin ||
-        origin === 'http://localhost:5173' ||
-        /^https:\/\/smart-study-coach.*\.vercel\.app$/.test(origin)
-      ) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: [
+      'http://localhost:5173',
+      'https://smart-study-coach-5ll2.vercel.app',
+    ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
